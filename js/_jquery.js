703f7074674745c12b839e6bde11e470dd9340be
Tony <me@maccuci.studio>
2022-10-24 07:27:51 -0300

    added profile container

--- js/_jquery.js
+++ js/_jquery.js
@@ -7,17 +7,21 @@ $(document).ready(function () {
         $("#home-container").show();
         $("#profile-container").hide();
         $("#contact-container").hide();
-    })
+    });
 
     $('#profile').click(function () {
         $("#home-container").hide();
         $("#profile-container").show();
         $("#contact-container").hide();
-    })
+    });
 
     $('#contact').click(function () {
         $("#home-container").hide();
         $("#profile-container").hide();
         $("#contact-container").show();
-    })
+    });
+
+    $('.message a').click(function () {
+        $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
+    });
 });
